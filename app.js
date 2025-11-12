24876d758437bebc89dfa6c231058a8bcee5946b
Prabhat Dhungana <PDhungana@lsua.edu>
2025-11-12 16:47:40 -0600

    Update homepage layout and add new article cards

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const firebaseConfig = {
   measurementId: "G-MSXGJMTJ2E"
 };
 
-/firebase.initializeApp(firebaseConfig);
+
+firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 
